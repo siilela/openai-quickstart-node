85ac3e852c3a9432f7046b1299d2099afea08a1f
Siiela Mogashoa <sumogashoa@shoprite.co.za>
2023-01-22 10:58:20 +0200

    crete web app for Gift Generator

---
+++ pages/gifts.js
@@ -0,0 +1,116 @@
+import Head from "next/head";
+import { useState } from "react";
+import styles from "./index.module.css";
+
+export default function Home() {
+  const [gender, setGender] = useState("man");
+  const [age, setAge] = useState(30);
+  const [priceMin, setPriceMin] = useState(50);
+  const [priceMax, setPriceMax] = useState(250);
+  const [hobbies, setHobbies] = useState("");
+  const [results,setResult]= useState("");
+  const [loading,setLoading] = useState(false);
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      const response = await fetch("/api/generate-gifts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ priceMin, priceMax, gender, age, hobbies }),
+      });
+
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw (
+          data.error ||
+          new Error(`Request failed with status ${response.status}`)
+        );
+      }
+
+      setResult(data.result.replaceAll("\n","<br />"));
+      setLoading(false);
+    } catch (error) {
+      // Consider implementing your own error handling logic here
+      console.error(error);
+      alert(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>OpenAI Quickstart</title>
+        <link rel="icon" href="/dog.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/gift.png" className={styles.icon} />
+        <h3>GIFT GENERATOR </h3>
+
+        <form onSubmit={onSubmit}>
+          <label>For who is the gift?</label>
+          <select
+            name="gender"
+            value={gender}
+            onChange={(e) => setGender(e.target.value)}
+          >
+            <option value="man">Man</option>
+            <option value="woman">Woman</option>
+          </select>
+
+          <label>Age</label>
+          <input
+            type="number"
+            min={1}
+            max={99}
+            name="age"
+            placeholder="Enter the age"
+            value={age}
+            onChange={(e) => setAge(Number.parseInt(e.target.value))}
+          />
+
+          <label>Price from</label>
+          <input
+            type="number"
+            min={1}
+            name="priceMin"
+            placeholder="Enter the minimum price"
+            value={priceMin}
+            onChange={(e) => setPriceMin(Number.parseInt(e.target.value))}
+          />
+
+          <label>Price to</label>
+          <input
+            type="number"
+            min={1}
+            name="priceMax"
+            placeholder="Enter the maximum price"
+            value={priceMax}
+            onChange={(e) => setPriceMax(Number.parseInt(e.target.value))}
+          />
+
+          <label>Hobbies</label>
+          <input
+            type="text"
+            name="hobbies"
+            placeholder="Enter the hobbies"
+            value={hobbies}
+            onChange={(e) => setHobbies(e.target.value)}
+          />
+          <input type="submit" value="Generate gift ideas" />
+        </form>
+        {loading && (
+          <div>
+            <h3>Looking for the best gift ideas 🎁 💡</h3>
+            <img src="/loading.gif" className={styles.loading} />
+          </div>
+        )}
+        <div className={styles.result} dangerouslySetInnerHTML={{__html:results}}/>
+      </main>
+    </div>
+  );
+}
